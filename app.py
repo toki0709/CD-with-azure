225016bb7c3c725e4a0fe809c435e0c33bca914d
toki0709 <tokitazwar71@gmail.com>
2022-08-21 13:23:04 +0200

    change in app.py

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
+import logging
 from flask import Flask, request, jsonify
 from flask.logging import create_logger
-import logging
-
 import pandas as pd
-from sklearn.externals import joblib
+import joblib
 from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
 LOG = create_logger(app)
 LOG.setLevel(logging.INFO)
+
 
 def scale(payload):
     """Scales Payload"""
@@ -17,6 +17,7 @@ def scale(payload):
     scaler = StandardScaler().fit(payload)
     scaled_adhoc_predict = scaler.transform(payload)
     return scaled_adhoc_predict
+
 
 @app.route("/")
 def home():
@@ -56,9 +57,9 @@ def predict():
 
     try:
         clf = joblib.load("boston_housing_prediction.joblib")
-    except:
+    except Exception as exp:
         LOG.info("JSON payload: %s json_payload")
-        return "Model not loaded"
+        return f"Model not loaded {exp}"
 
     json_payload = request.json
     LOG.info("JSON payload: %s json_payload")
@@ -67,6 +68,7 @@ def predict():
     scaled_payload = scale(inference_payload)
     prediction = list(clf.predict(scaled_payload))
     return jsonify({'prediction': prediction})
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
